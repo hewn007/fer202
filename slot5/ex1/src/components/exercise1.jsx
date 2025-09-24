0ffb6cc3a011575daeb8105186871d5fee5ba62e
hewn007 <quanmba007@gmail.com>
2025-09-24 09:14:51 +0700

    ex2

---
+++ slot5/ex1/src/components/exercise1.jsx
@@ -0,0 +1,14 @@
+export function Exercise1() {
+    const double = (x) => x * 2;
+    const isPositive = (x) => x > 0;
+  return (
+    <>
+    <p>Hello <strong>Exercise1</strong></p>
+    <h2>Chi tiet bai tap 1</h2>
+    <p></p>
+    </>
+    
+    
+    
+  );
+}
